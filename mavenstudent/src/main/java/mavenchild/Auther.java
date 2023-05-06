f0843e553fe38527643d75a468f43dfef6b5de7b
amalu <amalu@LAPTOP-3I9QMC8P>
2023-05-06 14:51:27 +0530

    update1

---
+++ mavenstudent/src/main/java/mavenchild/Auther.java
@@ -0,0 +1,17 @@
+package mavenchild;
+
+public class Auther 
+{
+	String autherName;
+	int age;
+	String place;
+	
+	Auther(String autherName,int age,String place)
+	{
+		this.autherName=autherName;
+		this.age=age;
+		this.place=place;
+	}
+	}
+
+		 
